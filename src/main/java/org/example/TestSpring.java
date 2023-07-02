1f2d15eeda0158b22cae3c844f6df732009568df
andrejhramogin <andrej.hramogin2@gmail.com>
2023-07-02 15:34:44 +0300

    1

---
+++ src/main/java/org/example/TestSpring.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+//        Music music = context.getBean("classical", Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        musicPlayer.playMusic();
+
+        context.close();
+    }
+}
